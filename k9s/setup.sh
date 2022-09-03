8fce08ebac7d8d2527bb15a69fbd3dbcfa62322f
Chenglong Hu <chenghu@microsoft.com>
2022-09-03 21:06:45 +0800

    remove k9s/config.yml

--- k9s/setup.sh
+++ k9s/setup.sh
@@ -1,5 +1,9 @@
 if [ ! -f "$HOME/.local/bin/k9s" ]; then
   mkdir -p $HOME/.local/bin
   curl -sS https://webi.sh/k9s | sh
+  mkdir -p $HOME/.config/k9s
   cp $HOME/bin/k9s/* $HOME/.config/k9s/
+  source $HOME/.config/envman/PATH.env
+  k9s version
 fi
+
