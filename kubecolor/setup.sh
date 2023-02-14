355620d8dded28b4a7386c66b42bfef34e063b4b
Chenglong Hu <chenghu@microsoft.com>
2023-02-14 12:52:24 +0800

    fix typo

--- kubecolor/setup.sh
+++ kubecolor/setup.sh
@@ -2,7 +2,7 @@ if [ ! -f "$HOME/.local/bin/kubecolor" ]; then
   cd $(mktemp -d)
   mkdir -p $HOME/.local/bin
   wget https://github.com/hidetatz/kubecolor/releases/download/v0.0.25/kubecolor_0.0.25_Linux_x86_64.tar.gz -O kubecolor.tar.gz
-  tar xvfz kubcolor.tar.gz
+  tar xvfz kubecolor.tar.gz
   cp kubecolor $HOME/.local/bin   
   source $HOME/.config/envman/PATH.env
   kubecolor version
