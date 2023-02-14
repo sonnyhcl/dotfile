d10fb67edceea9ba3db8d77b0e63404fcb345b81
Chenglong Hu <chenghu@microsoft.com>
2023-02-14 12:50:10 +0800

    cd /tmp/tmp.Ju1oCTz0bQ

--- k9s/setup.sh
+++ k9s/setup.sh
@@ -1,4 +1,5 @@
 if [ ! -f "$HOME/.local/bin/k9s" ]; then
+  cd $(mktemp -d)
   mkdir -p $HOME/.local/bin
   curl -sS https://webi.sh/k9s | sh
   mkdir -p $HOME/.config/k9s
--- kubecolor/setup.sh
+++ kubecolor/setup.sh
@@ -1,4 +1,5 @@
 if [ ! -f "$HOME/.local/bin/kubecolor" ]; then
+  cd $(mktemp -d)
   mkdir -p $HOME/.local/bin
   wget https://github.com/hidetatz/kubecolor/releases/download/v0.0.25/kubecolor_0.0.25_Linux_x86_64.tar.gz -O kubecolor.tar.gz
   tar xvfz kubcolor.tar.gz
