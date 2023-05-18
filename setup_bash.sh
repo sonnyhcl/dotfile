6b92442fefbc4722e3c304b6ced335014ac08d35
sonnyhcl <sonnyhcl@gmail.com>
2023-05-18 21:13:42 +0800

    setup bash with azure-cli/kubectl/istioctl

--- setup_bash.sh
+++ setup_bash.sh
@@ -23,3 +23,29 @@ cp vimrc $HOME/.vimrc
 mkdir -p $HOME/bin
 cp -r k9s $HOME/bin
 cp -r kubecolor $HOME/bin
+
+## Setup azure-cli
+command_name="az"
+if ! command_loc="$(type -p "$command_name")" || [[ -z $command_loc ]]; then
+  curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+fi
+az version
+
+## Setup kubectl
+command_name="kubectl"
+if ! command_loc="$(type -p "$command_name")" || [[ -z $command_loc ]]; then
+  sudo apt update
+  sudo apt install -y ca-certificates curl apt-transport-https
+  sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+  sudo apt update
+  sudo apt install -y kubectl
+fi
+kubectl version --client=true
+
+## Setup istioctl
+command_name="istioctl"
+if ! command_loc="$(type -p "$command_name")" || [[ -z $command_loc ]]; then
+  curl -sL https://istio.io/downloadIstioctl | sh -
+fi
+istioctl version --remote=false
